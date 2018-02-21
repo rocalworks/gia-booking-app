10fd6d3be8380f19ba589b5c4fd3d68a02205551
Krisha Orge (Ryxie) <rocalworks@gmail.com>
2018-02-21 17:32:21 +0800

    Star rating! (basic)

--- src/sections/House.js
+++ src/sections/House.js
@@ -3,10 +3,26 @@ import { ButtonGroup, Button } from 'react-bootstrap';
 
 class House extends Component {
     render() {
-        { /* TODO: Change to carousel background (up to 5 images) */ }
         const styles = {
             backgroundImage: "url(" + this.props.house.imageURL + ")"
         }
+        
+        let review = this.props.house.reviews;
+        let i = 0;
+        const starReview = [];
+                                
+        while(i < 5) {
+            if(review === 0.5) {
+                starReview.push(<i key={review} className="icon-star-half-empty"></i>); 
+            } else if (review === 0){
+                starReview.push(<i key={review} className="icon-star-empty"></i>); 
+            } else {
+                starReview.push(<i key={review} className="icon-star"></i>);
+            }
+            
+            review = review - 1;
+            i++;
+        };
         
         return(
             <div className="house-item">
@@ -17,8 +33,11 @@ class House extends Component {
                         </a>
                         { /* TODO: Make it star ratings instead*/ }
                         <a className="house-reviews">
-                            {this.props.house.reviews} Reviews
+                            {starReview}
                         </a>
+                        <div>
+                            
+                        </div>
                     </div>
                 </div>
                 <div className="house-highlights">
